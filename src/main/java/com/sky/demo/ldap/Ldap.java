06123aa9e91e6136efebaa1796777ce682d645c6
rg <uuuzombie@gmail.com>
2017-02-09 18:04:46 +0800

    init gui

---
+++ src/main/java/com/sky/demo/ldap/Ldap.java
@@ -0,0 +1,34 @@
+package com.sky.demo.ldap;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by user on 17/2/9.
+ */
+public class Ldap {
+    private JButton Sync;
+    private JPanel panelMain;
+
+    public Ldap() {
+        Sync.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Sync...");
+            }
+        });
+    }
+
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Ldap");
+        jFrame.setContentPane(new Ldap().panelMain);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setSize(600,500);
+        jFrame.setPreferredSize(new Dimension(600,500));
+        jFrame.pack();
+        jFrame.setVisible(true);
+    }
+}
